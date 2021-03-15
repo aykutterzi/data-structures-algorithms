bbfea004c1f97bd01abf1585b91e4d18f2e75d05
Aykut Terzi <terziaykut@hotmail.com>
2021-03-15 22:08:17 +0300

    Add sliding window

---
+++ Problem-Solving-Patterns/Sliding-Window/maxSubarraySum.js
@@ -0,0 +1,22 @@
+/*Write a function called maxSubarraySum which accepts an array of integers and a number called n. The function should calculate the maximum sum of n consecutive elements in the array.*/
+
+function maxSubarraySum(arr, num) {
+  if (num > arr.length) {
+    return null;
+  }
+  let max = -Infinity;
+  for (let i = 0; i < arr.length - num + 1; i++) {
+    let temp = 0;
+    for (let j = 0; j < num; j++) {
+      temp += arr[i + j];
+    }
+    if (temp > max) {
+      max = temp;
+    }
+  }
+  console.log(max);
+}
+
+maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2); // 10
+maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 4); // 17
+maxSubarraySum([4, 2, 1, 6], 1); // 6
