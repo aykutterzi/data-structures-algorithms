ec683f6c88b54533c4af6be6cd66cb2aee250329
Aykut Terzi <terziaykut@hotmail.com>
2021-03-15 18:05:38 +0300

    Add isAnagram

---
+++ Problem-Solving-Patterns/Frequency-Counter/isAnagram.js
@@ -0,0 +1,29 @@
+/* Given two strings, write a function to determine if the second string is an anagram of the first. An anagram is a word, phrase, or name formed by rearranging the letters of another, such as listen, from silent */
+
+// Assume all strings are lowercase letters.
+
+function isAnagram(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  const lookup = {};
+
+  for (let i = 0; i < str1.length; i++) {
+    let letter = str1[i];
+    // if letter exists, increment, otherwise st to 1
+    lookup[letter] ? (lookup[letter] += 1) : (lookup[letter] = 1);
+  }
+
+  for (let i = 0; i < str2.length; i++) {
+    let letter = str2[i];
+    // can't find letter or letter is zero then it's not an anagram
+    if (!lookup[letter]) {
+      return false;
+    } else {
+      lookup[letter] -= 1;
+    }
+  }
+
+  return true;
+}
